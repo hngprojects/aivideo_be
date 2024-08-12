76299bc1ca5de46499519ddb099d03475a909789
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-12 19:15:13 +0100

    feat: add CRUD endpoints for FAQ feature

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from api.v1.routes.auth import auth
 from api.v1.routes.user import user_router
 from api.v1.routes.ai_tools.summary import summary
+from api.v1.routes.faq import faq
 from tests.run_all_test import test_rout
 
 api_version_one = APIRouter(prefix="/api/v1")
@@ -10,3 +11,4 @@ api_version_one.include_router(auth)
 api_version_one.include_router(user_router)
 api_version_one.include_router(test_rout)
 api_version_one.include_router(summary)
+api_version_one.include_router(faq)
---
+++ api/v1/routes/faq.py
@@ -0,0 +1,141 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from api.db.database import get_db
+from api.utils.success_response import success_response
+from api.v1.models.user import User
+from api.v1.services.user import user_service
+from api.v1.services.faq import faq_service
+from api.v1.schemas.faq import CreateFAQ, UpdateFAQ, FAQBase
+import logging
+
+faq = APIRouter(prefix="/faqs", tags=["FAQs"])
+
+
+@faq.get("", response_model=success_response, status_code=200)
+async def get_all_faqs(db: Session = Depends(get_db),):
+    """Endpoint to get all FAQs
+
+    Args:
+        db (Session, optional): The db session object. Defaults to Depends(get_db).
+    """    
+    
+    faqs = faq_service.fetch_all(db=db)
+    faqs_filtered = list(
+        map(lambda x: FAQBase.model_validate(x), faqs)
+    )
+    if len(faqs_filtered) == 0:
+        faqs_filtered = None
+
+    return success_response(
+        status_code=200,
+        message="FAQs retrieved successfully",
+        data=jsonable_encoder(faqs_filtered),
+    )
+
+
+@faq.post("", response_model=success_response, status_code=201)
+async def create_faq(
+    schema: CreateFAQ,
+    db: Session = Depends(get_db),
+    current_admin: User = Depends(user_service.get_current_super_admin),
+):
+    """Endpoint to create a new FAQ. Only accessible to superadmins
+
+    Args:
+        schema (CreateFAQ): Request Body for creating faq
+        db (Session, optional): The db session object. Defaults to Depends(get_db).
+        current_admin (User, optional): Admin User. Defaults to Depends(user_service.get_current_super_admin).
+
+    Returns:
+        success_response
+    """
+    faq = faq_service.create(db, schema=schema)
+
+    if schema.answer.strip() == '' or schema.category.strip() == '' or schema.question.strip() == '':
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid request body")
+    
+    logging.info(f'Creating new FAQ. ID: {faq.id}.')
+    return success_response(
+        data=jsonable_encoder(FAQBase.model_validate(faq)),
+        message="Successfully created FAQ",
+        status_code=status.HTTP_201_CREATED,
+    )
+
+
+@faq.get("/{id}", response_model=success_response, status_code=200)
+async def get_single_faq(id: str, db: Session = Depends(get_db)):
+    """Endpoint to get a single FAQ
+
+    Args:
+        id (str): Faq ID
+        db (Session, optional): Defaults to Depends(get_db).
+
+    Raises:
+        HTTPException: 404 NOT FOUND (Faq to be retrieved cannot be found)
+    """
+    faq = faq_service.fetch(db, faq_id=id)
+
+    if faq == None:
+        raise HTTPException(status_code=404, detail="FAQ not found")
+
+    return success_response(
+        data=jsonable_encoder(FAQBase.model_validate(faq)),
+        message="Successfully fetched FAQ",
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@faq.patch("/{id}", response_model=success_response, status_code=200)
+async def update_faq(
+    id: str,
+    schema: UpdateFAQ,
+    db: Session = Depends(get_db),
+    current_admin: User = Depends(user_service.get_current_super_admin),
+):
+    """Endpoint to update an FAQ. Only accessible to superadmins
+    Args:
+        id (str)
+        schema (UpdateFAQ)
+        db (Session, optional). Defaults to Depends(get_db).
+        current_admin (User, optional). Defaults to Depends(user_service.get_current_super_admin).
+
+    Raises:
+        HTTPException: 404 NOT FOUND (Faq to be retrieved cannot be found)
+    """
+    faq = faq_service.update(db, faq_id=id, schema=schema)
+
+    if faq == None:
+        raise HTTPException(status_code=404, detail="FAQ not found")
+
+    logging.info(f'Updating FAQ. ID: {faq.id}')
+    return success_response(
+        data=jsonable_encoder(FAQBase.model_validate(faq)),
+        message="FAQ updated successfully",
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@faq.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_faq(
+    id: str,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(user_service.get_current_super_admin),
+):
+    """Endpoint to delete an FAQ. Only accessible to superadmins
+
+    Args:
+        id (str)
+        db (Session, optional): Defaults to Depends(get_db).
+        current_user (User, optional): Defaults to Depends(user_service.get_current_super_admin).
+
+    Raises:
+        HTTPException: 404 NOT FOUND (Faq to be deleted cannot be found)
+    """
+    status = faq_service.delete(db, faq_id=id)
+
+    if status == False:
+        raise HTTPException(status_code=404, detail="FAQ not found")
+    
+    logging.info(f'Deleted FAQ. ID: {id}.')
