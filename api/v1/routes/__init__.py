ef0c5ba3c075c376a706b82385211e8507522fd8
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-08-13 07:42:59 +0100

    feat: set up and integrate celery and rabbitmq

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from api.v1.routes.auth import auth
 from api.v1.routes.user import user_router
 from api.v1.routes.ai_tools.summary import summary
+from api.v1.routes.ai_tools.background_task import background_router
 from tests.run_all_test import test_rout
 
 api_version_one = APIRouter(prefix="/api/v1")
@@ -10,3 +11,4 @@ api_version_one.include_router(auth)
 api_version_one.include_router(user_router)
 api_version_one.include_router(test_rout)
 api_version_one.include_router(summary)
+api_version_one.include_router(background_router)
