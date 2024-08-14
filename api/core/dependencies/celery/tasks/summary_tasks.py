71c0df8c5d93980b45db9af424b569ae5eb9aea8
laban254 <labanrotich6544@gmail.com>
2024-08-14 16:50:47 +0300

    feat: added task for audio transcription and translation

--- api/core/dependencies/celery/tasks/summary_tasks.py
+++ api/core/dependencies/celery/tasks/summary_tasks.py
@@ -2,6 +2,8 @@ from celery import shared_task
 
 from api.core.dependencies.celery.celery_app import worker
 from api.v1.services.ai_tools.summary import summary_service
+from api.v1.services.ai_tools.audio_transcriber import transcribe_audio, translate_text
+
 from api.db.database import get_db
 
 db = next(get_db())
@@ -12,3 +14,13 @@ def generate_pdf_summary_task(pdf_file):
 
     summary = summary_service.summarize_pdf(pdf_file)
     return summary
+
+@worker.task()
+def transcribe_audio_task(file_path: str):
+    """Celery task for transcribing audio."""
+    return transcribe_audio(file_path)
+
+@worker.task()
+def translate_text_task(text: str, target_language: str):
+    """Celery task for translating text."""
+    return translate_text(text, target_language)
