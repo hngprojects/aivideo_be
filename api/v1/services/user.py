92d89f224425eac19a7022873d32b5738976d02f
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-08-14 16:12:27 +0100

    feat: update the last_login field, every time a request is made

--- api/v1/services/user.py
+++ api/v1/services/user.py
@@ -423,6 +423,7 @@ class UserService(Service):
 
         token = self.verify_access_token(access_token, credentials_exception)
         user = db.query(User).filter(User.id == token.id).first()
+        user.update_last_login()
 
         return user
 
