2aade646454bf52d2160c3a07b6a75ca1916b734
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-08-14 18:57:59 +0100

    chore: added video tasks to celery app

--- api/core/dependencies/celery/celery_app.py
+++ api/core/dependencies/celery/celery_app.py
@@ -5,7 +5,8 @@ worker = Celery(
     "worker",
     broker=settings.CELERY_BROKER_URL,
     backend=f'db+{settings.DB_URL}',
-    include=['api.core.dependencies.celery.tasks.summary_tasks']
+    include=['api.core.dependencies.celery.tasks.summary_tasks',
+             'api.core.dependencies.celery.tasks.video_tasks',]
 )
 
 worker.conf.update(
