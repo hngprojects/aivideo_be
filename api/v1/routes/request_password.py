6db547e0c895ca627b94938eb1180cebb099b0e8
SundayMba <sundinoh@gmail.com>
2024-08-14 02:49:55 +0100

    feat: added test cases and renamed auth route

--- api/v1/routes/request_password.py
+++ api/v1/routes/request_password.py
@@ -5,19 +5,21 @@ from api.db.database import get_db as get_session
 from api.v1.services.request_pwd import reset_service
 import logging
 from api.utils.success_response import success_response
+from api.v1.services.user import user_service
+from api.v1.models.user import User
 
 pwd_reset = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 # generate password reset link
-@pwd_reset.post("/request-password-reset")
-async def request_reset_link(
+@pwd_reset.post("/request-forget-password")
+async def request_forget_password(
     reset_schema: RequestEmail,
     request: Request,
     background_tasks: BackgroundTasks,
     db: Session = Depends(get_session),
 ):
-    url = "api/v1/auth/reset-password"
+    url = "api/v1/auth/forget-password"
     template_file = "reset_password.html"
     subject = "HNG11 PASSWORD RESET"
     data =  await reset_service.create(reset_schema, request, db, background_tasks,
@@ -25,18 +27,27 @@ async def request_reset_link(
     return success_response(**data)
 
 # process password link
-@pwd_reset.get("/reset-password")
-async def process_reset_link(
+@pwd_reset.get("/forget-password")
+async def process_forget_password_link(
     token: str = Query(...), db: Session = Depends(get_session)
 ):
     return reset_service.process_reset_link(token, db)
 
 
 # change the password
-@pwd_reset.post("/reset-password")
-async def reset_password(
+@pwd_reset.post("/forget-password")
+async def forget_password(
     data: ResetPassword,
     token: str = Query(...),
     session: Session = Depends(get_session),
 ):
     return reset_service.reset_password(data, token, session)
+
+# change the password
+@pwd_reset.post("/reset-password")
+async def reset_password(
+    data: ResetPassword,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(user_service.get_current_user)
+):
+    return reset_service.reset_user_password(data, session, current_user)
