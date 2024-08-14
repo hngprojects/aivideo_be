739cb803464aeb1babd5d0fb6a09011107264b2f
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-14 09:11:44 +0100

    chore: Moved raising of HTTPException to faq_service file for retrieve endpoint

--- api/v1/routes/faq.py
+++ api/v1/routes/faq.py
@@ -29,9 +29,7 @@ async def create_faq(
         success_response
     """
     faq = faq_service.create(db, schema=schema)
-
-    if schema.answer.strip() == '' or schema.category.strip() == '' or schema.question.strip() == '':
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid request body")
+    
     
     logging.info(f'Creating new FAQ. ID: {faq.id}.')
     return success_response(
@@ -74,12 +72,8 @@ async def get_single_faq(id: str, db: Session = Depends(get_db)):
     """
     faq = faq_service.fetch(db, faq_id=id)
 
-    if faq == None:
-        raise HTTPException(status_code=404, detail="FAQ not found")
-
     return success_response(
         data=jsonable_encoder(FAQBase.model_validate(faq)),
         message="Successfully fetched FAQ",
         status_code=status.HTTP_200_OK,
-
     )
