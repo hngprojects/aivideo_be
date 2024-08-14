e31ca13df9bb1c96c8b962d11865fb8fa4ccc061
AKINNUOYE TIMOTHY <akinnuoyet@gmail.com>
2024-08-14 17:57:57 +0100

    fix:merged dev

--- api/v1/routes/job.py
+++ api/v1/routes/job.py
@@ -13,6 +13,7 @@ from api.v1.services.job import job_service
 
 job = APIRouter(prefix="/jobs", tags=["Jobs"])
 
+
 # Get all jobs
 @job.get("/", response_model=List[JobResponse], status_code=status.HTTP_200_OK)
 async def get_all_jobs():
@@ -20,17 +21,17 @@ async def get_all_jobs():
     jobs = job_service.fetch_all_jobs()
     return jobs
 
+
 # Get a job by its ID
 @job.get("/{job_id}/update_result", response_model=JobResponse)
 async def update_job_result(job_id: str, background_tasks: BackgroundTasks):
-    '''Endpoint to update the job result after the task is completed'''
-    
+    """Endpoint to update the job result after the task is completed"""
+
     def update_result_task(job_id: str):
         job_service.update_job_result(job_id)
 
     background_tasks.add_task(update_result_task, job_id)
     return job_service.fetch_by_job_id(job_id)
-
 
 
 @job.get("/activity")
