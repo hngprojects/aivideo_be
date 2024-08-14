0b79c856e996216f700655ff8c37540f4c66ba52
segunUb18 <eeomo@yahoo.com>
2024-08-14 02:16:20 +0100

    :sparkles: implement endpoint to fetch current user profile

--- api/v1/routes/profile.py
+++ api/v1/routes/profile.py
@@ -11,6 +11,24 @@ from api.utils.success_response import success_response
 
 profile = APIRouter(prefix='/profile', tags=['Profiles'])
 
+
+@profile.get("/me", response_model=success_response)
+def get_current_user_profile(
+    db: Session = Depends(get_db), 
+    current_user: User = Depends(user_service.get_current_user)
+    ):
+    """
+    Get the profile of the currently authenticated user
+    """
+    profile = profile_service.fetch_by_user_id(db, user_id=current_user.id)
+    
+    return success_response(
+        status_code=status.HTTP_200_OK,
+        message="User Profile retrieved Successfully!!!",
+        data=profile
+    )
+
+
 @profile.put('', status_code=status.HTTP_200_OK, response_model=ProfileBase)
 def update_user_profile(
     schema: ProfileCreateUpdate,
