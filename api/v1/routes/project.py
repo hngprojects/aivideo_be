18111d9b18ad635ca9807a048595f37844481caa
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-14 10:19:47 +0100

    chore: Added endpoints and schema for creating new projects

---
+++ api/v1/routes/project.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from api.db.database import get_db
+from api.utils.success_response import success_response
+from api.v1.models.user import User
+from api.v1.services.user import user_service
+from api.v1.services.project import project_service
+from api.v1.schemas.project import CreateFullProjectSchema, AddFullProjectSchema, ProjectCreateResponseSchema
+import logging
+
+project = APIRouter(prefix="/projects", tags=["Projects"])
+
+@project.post("", response_model=success_response, status_code=201)
+async def create_project(
+    schema: CreateFullProjectSchema,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(user_service.get_current_user),
+):
+    """Endpoint to create a new projects. Only accessible to authenticated users
+    Args:
+        schema (CreateFullProjectSchema): Request Body for creating projects
+        db (Session, optional): The db session object. Defaults to Depends(get_db).
+        current_user (User, optional): User. Defaults to Depends(user_service.get_current_user).
+    Returns:
+        success_response
+    """
+    if schema.title.strip() == '' or schema.project_type.strip() == '':
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid request body")
+
+    full_project = AddFullProjectSchema(user_id=current_user.id, **schema.model_dump())
+    
+    new_project = project_service.create(db, full_project)
+
+    
+    logging.info(f'Creating new Project. ID: {new_project.id}.')
+    return success_response(
+        data=jsonable_encoder(ProjectCreateResponseSchema.model_validate(new_project)),
+        message="Successfully created project",
+        status_code=status.HTTP_201_CREATED,
+    )
