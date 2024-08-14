cfd0c74e8c63c7bf595d827426e7e3248c0b0d9c
laban254 <labanrotich6544@gmail.com>
2024-08-14 17:43:21 +0300

    feat: add checking  of  file type

--- api/v1/routes/ai_tools/audio_transcriber.py
+++ api/v1/routes/ai_tools/audio_transcriber.py
@@ -1,15 +1,26 @@
-# audio_transcriber.py
 from fastapi import FastAPI, UploadFile, File, HTTPException, APIRouter
 from api.v1.schemas.audio_transcriber import TranslationRequest
-from api.core.dependencies.celery.tasks.summary_tasks import  transcribe_audio_task, translate_text_task
+from api.core.dependencies.celery.tasks.summary_tasks import transcribe_audio_task, translate_text_task
 from api.v1.services.job import job_service
+import mimetypes
 
 AUDIOFILE = "audio.mp3"  
 audio = APIRouter(prefix="/tools/audio-transcribe", tags=["Tools"])
 
+def is_audio_file(filename: str, content_type: str) -> bool:
+    """Check if the file is an audio file based on the filename and content type."""
+    audio_content_types = [
+        'audio/mpeg', 'audio/mp3', 'audio/wav', 'audio/ogg', 'audio/aac'
+    ]
+    _, ext = mimetypes.guess_type(filename)
+    return content_type in audio_content_types and (ext == 'mp3' or ext == 'wav')
+
 @audio.post("/upload/")
 async def upload_audio(file: UploadFile = File(...)):
     """Upload and transcribe audio file."""
+    if not is_audio_file(file.filename, file.content_type):
+        raise HTTPException(status_code=400, detail="Invalid file type. Only audio files are allowed.")
+    
     try:
         file_path = AUDIOFILE
         with open(file_path, "wb") as buffer:
