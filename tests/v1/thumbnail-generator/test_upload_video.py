23885d7a2d01c55417a5e6f519cafdad4d304f57
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-08-15 06:51:05 +0100

    chore: test upload video sucess change

--- tests/v1/thumbnail-generator/test_upload_video.py
+++ tests/v1/thumbnail-generator/test_upload_video.py
@@ -17,23 +17,23 @@ class MockSettings:
 settings = MockSettings()
 
 
-# @patch('api.core.dependencies.celery.tasks.video_tasks.upload_video_task.delay')
-# @patch('api.utils.files.upload_file', return_value='./media/uploads/videos/video-mocked.mov')
-# @patch('os.path.exists', return_value=True)
-# @patch('os.makedirs')
-# @patch('builtins.open', new_callable=mock_open)
-# def test_upload_video_success(mock_open, mock_makedirs, mock_exists, mock_upload_file, mock_task):
-#     mock_task.return_value.id = 'mock-task-id'
-#     mock_file = MagicMock()
-#     mock_file.filename = 'video.mov'
-#     mock_file.read.return_value = b'test video content'
+@patch('api.core.dependencies.celery.tasks.video_tasks.upload_video_task.delay')
+@patch('api.utils.files.upload_file', return_value='./media/uploads/videos/video-mocked.mov')
+@patch('os.path.exists', return_value=True)
+@patch('os.makedirs')
+@patch('builtins.open', new_callable=mock_open)
+def test_upload_video_success(mock_open, mock_makedirs, mock_exists, mock_upload_file, mock_task):
+    mock_task.return_value.id = 'mock-task-id'
+    mock_file = MagicMock()
+    mock_file.filename = 'video.mov'
+    mock_file.read.return_value = b'test video content'
 
-#     response = client.post(
-#         '/api/v1/thumbnails/upload',
-#         files={'file': ('video.mov', mock_file.read())}
-#     )
+    response = client.post(
+        '/api/v1/thumbnails/upload',
+        files={'file': ('video.mov', mock_file.read())}
+    )
 
-#     assert response.status_code == 200
+    assert response.status_code == 200
 
 
 
