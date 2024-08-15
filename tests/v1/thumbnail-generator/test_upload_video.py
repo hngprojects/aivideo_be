5cea5c8e1f372f5fffecc6d8219ce18988c5e214
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-08-15 06:14:45 +0100

    chore: breaking test file update

--- tests/v1/thumbnail-generator/test_upload_video.py
+++ tests/v1/thumbnail-generator/test_upload_video.py
@@ -17,12 +17,13 @@ class MockSettings:
 settings = MockSettings()
 
 
+@patch('api.core.dependencies.celery.tasks.video_tasks.upload_video_task.delay')
 @patch('api.utils.files.upload_file', return_value='./media/uploads/videos/video-mocked.mov')
 @patch('os.path.exists', return_value=True)
 @patch('os.makedirs')
 @patch('builtins.open', new_callable=mock_open)
-def test_upload_video_success(mock_open, mock_makedirs, mock_exists, mock_upload_file):
-
+def test_upload_video_success(mock_open, mock_makedirs, mock_exists, mock_upload_file, mock_task):
+    mock_task.return_value.id = 'mock-task-id'
     mock_file = MagicMock()
     mock_file.filename = 'video.mov'
     mock_file.read.return_value = b'test video content'
