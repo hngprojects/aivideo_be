a213c3791fa958be27a6036d50c3787933847a44
segunUb18 <eeomo@yahoo.com>
2024-08-15 01:57:41 +0100

    :art: handle the change password service properly

--- api/v1/services/user.py
+++ api/v1/services/user.py
@@ -499,7 +499,7 @@ class UserService(Service):
         """Endpoint to change the user's password"""
         
         # Check if the user has an existing password
-        if user.password == "":
+        if not user.password:
             # user signed up via social authentication (Google/Facebook)
             if old_password:
                 raise HTTPException(
