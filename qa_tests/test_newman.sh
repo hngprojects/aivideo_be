d26c3f9c4d28084ea6aec3c2707e7c7f04e12ba0
KronosRetroMech <kronosretromech@gmail.com>
2024-08-16 11:48:22 +0100

    Added the Newman test report for Login Boilerplate

--- qa_tests/test_newman.sh
+++ qa_tests/test_newman.sh
@@ -12,4 +12,6 @@ newman run aivideo_be/qa_tests/regression/Payments-Flow_one-off-subscription.pos
 
 # newman run your-collection.json -r htmlextra --reporter-htmlextra-export newman-reports/report.html
 
+# Run Newman with HTMLextra reporter
+newman run aivideo_be/qa_tests/regression/Stage7_Login_Boilerplate.postman_collection.json -r htmlextra
 
