5c2b81f53c76be616b8b88c1551bc149119de0cd
Waptech <abdulfataiyakub07@gmail.com>
2024-08-16 09:47:29 +0100

    video subtitles

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -25,6 +25,7 @@ from api.v1.routes.ai_tools.audio_transcriber import audio
 from api.v1.routes.dashboard import dashboard
 
 from api.v1.routes.ai_tools.thumbnail import thumbnail_router
+from api.v1.routes.ai_tools.video_subtitles import video_subtitles_router
 
 api_version_one = APIRouter(prefix="/api/v1")
 
@@ -57,3 +58,4 @@ api_version_one.include_router(video_summary)
 api_version_one.include_router(audio)
 api_version_one.include_router(dashboard)
 api_version_one.include_router(thumbnail_router)
+api_version_one.include_router(video_subtitles_router)
