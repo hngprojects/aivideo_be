c93edf5a8b4aae19dabb92139ace5f4eff7b7393
Leo Hyperion <hyperiondigit@gmail.com>
2024-08-16 10:44:12 +0100

    Added the newman report json file for the Sigup feature (boilerplate)

--- qa_tests/test_newman.sh
+++ qa_tests/test_newman.sh
@@ -12,4 +12,5 @@ newman run aivideo_be/qa_tests/regression/Payments-Flow_one-off-subscription.pos
 
 # newman run your-collection.json -r htmlextra --reporter-htmlextra-export newman-reports/report.html
 
-
+# Run Newman with HTML reporter
+newman run aivideo_be/qa_tests/regression/Stage_7_Anastasia.postman_collection2.json -r htmlextra
