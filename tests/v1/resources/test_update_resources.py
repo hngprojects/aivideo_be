f766c6f951e3c0073523ee1fec9f5248dedea17f
utibe solomon <utibesolomon6@gmail.com>
2024-08-21 17:37:03 +0100

    feat added new endpoint to update resources

--- tests/v1/resources/test_update_resources.py
+++ tests/v1/resources/test_update_resources.py
@@ -37,14 +37,16 @@ client = TestClient(app)
 
 def test_update_user(db_session_mock, mock_user):
     app.dependency_overrides[user_service.get_current_super_admin] = mock_user
-    app.dependency_overrides[get_db] = db_session_mock
-    body = {
-        "title" : "I love titles"
-        }
+    app.dependency_overrides[get_db] = lambda : db_session_mock
+    test_resource_req_body = {
+    "title": "TTest title?",
+    "content": "TAnswer",
+    "image_url": "random.com",
+    }
  
     resource = mock_resource()
     db_session_mock.get.return_value = resource
     response = client.patch(f'/api/v1/resources/{resource.id}',
-                            json=body)
+                            json=test_resource_req_body)
     print(response.json())
     assert response.status_code == 200
