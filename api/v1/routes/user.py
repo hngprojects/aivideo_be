7cbbfb1cc82028b33b2bfc9f500061dcf766c52e
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-08-22 20:09:57 +0100

    fix: fix state_map logic

--- api/v1/routes/user.py
+++ api/v1/routes/user.py
@@ -89,21 +89,47 @@ def update_current_user(
     )
 
 
+################ SSE ENDPOINT FOR USER STATISTICS ###################
+
 # total number of connected clients
 connections = 0
-# {"connection": "state"}
+# {"connection": ["state", "active_status"]}
 state_map = {}
+
+
+def reset_connection_active_status():
+    """reset the active status for each open connection tracked by the state_map"""
+    if state_map:
+        for key, value in state_map.items():
+            state_map[key][1] = 0
+
+
+def remove_inactive_connections():
+    """remove all inactive connections from the state_map"""
+    connections_to_remove = []
+    if state_map:
+        for key, value in state_map.items():
+            if state_map[key][1] == 0:
+                connections_to_remove.append(key)
+        for connection in connections_to_remove:
+            del state_map[connection]
+
 
 @event.listens_for(User, "after_insert")
 @event.listens_for(User, "after_update")
 def orm_event_listener(mapper, connection, target):
-    # once a change in the db is detected
-    # fill each connection in the state_map with 1
-    for i in range(connections):
-        state_map[f"connection_{i}"] = 1
+    """listen for db updates and update the state_map"""
 
-    # - state -> 1 when there is an update
-    # - state -> 0 when there is no update
+    # once a change in the db is detected
+    # clear all inactive connections from state_map
+    # fill each connection in the state_map with [1, 1]
+    # [update_state, active_state]
+
+    remove_inactive_connections()
+
+    if state_map:
+        for key, value in state_map.items():
+            state_map[key] = [1, 1]
 
 
 async def event_generator(request: Request, db: Session):
@@ -112,29 +138,24 @@ async def event_generator(request: Request, db: Session):
     connections += 1
     map_key = f"connection_{connection_index}"
 
+    reset_connection_active_status()
+
     # initialize current connection state
-    state_map[map_key] = 1
+    state_map[map_key] = [1, 1]
 
     while True:
-        # close connection is client disconnects
-        if await request.is_disconnected():
-            print(f"client {request.client.host} disconnected")
-            connections -= 1
-            del state_map[map_key]
-            break
-
+        # mark the current connection as active
+        state_map[map_key][1] = 1
         # check if state map is non empty
-        if state_map:
-            if state_map[map_key] == 1:
-                # send a message if state is 1
-                data = json.dumps(user_service.get_users_statistics(db))
+        if state_map[map_key][0] == 1:
+            # send a message if update_state is 1
+            data = json.dumps(user_service.get_users_statistics(db))
 
-                # reset current connection state to 0
-                state_map[map_key] = 0
+            # reset current connection update_state to 0
+            state_map[map_key][0] = 0
 
-                yield {"event": "statsUpdate", "data": data}
+            yield {"event": "statsUpdate", "data": data}
         await asyncio.sleep(1)
-
 
 
 @user_router.get(
@@ -146,6 +167,8 @@ def get_user_statistics(
 ):
     """Endpoint to fetch all user statistics"""
     return EventSourceResponse(event_generator(request, db))
+
+#########################################
 
 @user_router.get("/export/csv", status_code=status.HTTP_200_OK)
 def export_csv(
@@ -359,6 +382,5 @@ def change_password(
     )
 
     return success_response(
-        status_code=status.HTTP_200_OK,
-        message="Password changed successfully!!"
+        status_code=status.HTTP_200_OK, message="Password changed successfully!!"
     )
