abfeb91d3bcb1c20c6d4005825b4ca0099b17ab1
Ayobamidele <ayobamideleewetuga@gmail.com>
2024-08-22 16:33:39 +0100

    feat: unified download endpoint

--- api/v1/routes/download.py
+++ api/v1/routes/download.py
@@ -9,16 +9,13 @@ from api.v1.schemas.ai_tools.talking_avatar import DownloadRequest
 download_router = APIRouter(prefix="/download", tags=["Download"])
 
 
-
-
-
 @download_router.post("")
 async def download_file(schema: DownloadRequest):
     try:
         # Fetch the file from the URL
         response = requests.get(schema.file_url, stream=True)
         response.raise_for_status()  # Check for errors in the response
-        file_name = f"convey_{schema.file_url.split("/")[-1]}"
+        file_name = f'convey_{schema.file_url.split("/")[-1]}'
         with open(file_name, "wb") as video_file:
             video_file.write(response.content)
 
