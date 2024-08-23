1c3a28265968dbea0b5329dca2603a10c89514c4
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-23 04:36:55 +0100

    chore: Added extra array column to usage store table

--- api/v1/models/usage_store.py
+++ api/v1/models/usage_store.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from sqlalchemy import Column, DateTime, Integer, String
 
 from api.v1.models.base_model import BaseTableModel
-
+from sqlalchemy.dialects.postgresql import ARRAY
 
 class UsageStore(BaseTableModel):
     __tablename__ = "usage_store"
@@ -13,3 +13,4 @@ class UsageStore(BaseTableModel):
     ip_address = Column(String, index=True, nullable=False)
     tool_access_count = Column(Integer, default=0)
     last_accessed = Column(DateTime, default=datetime.utcnow, nullable=False)
+    tools_accessed = Column(ARRAY(String), nullable=False)
