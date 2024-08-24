737dec06513a4d8b59bb0a692bfc1dc941bfe09f
Ayobamidele <ayobamideleewetuga@gmail.com>
2024-08-24 17:44:47 +0100

    feat: creates models to track user usage.

--- api/v1/models/usage_store.py
+++ api/v1/models/usage_store.py
@@ -2,8 +2,8 @@
 
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, String
-
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from api.v1.models.base_model import BaseTableModel
 from sqlalchemy.dialects.postgresql import ARRAY
 
@@ -14,3 +14,13 @@ class UsageStore(BaseTableModel):
     tool_access_count = Column(Integer, default=0)
     last_accessed = Column(DateTime, default=datetime.utcnow, nullable=False)
     tools_accessed = Column(ARRAY(String), nullable=True)
+
+class UserUsageStore(BaseTableModel):
+    __tablename__ = "user_usage_store"
+    
+    user_id = Column(String, ForeignKey('users.id', ondelete="CASCADE"))
+    tool_access_count = Column(Integer, default=0)
+    last_accessed = Column(DateTime, default=datetime.utcnow, nullable=False)
+    tools_accessed = Column(ARRAY(String), nullable=True)
+    
+    user = relationship('User', back_populates='usage_stored')
