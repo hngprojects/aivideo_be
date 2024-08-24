1d404098431e1f13230f70d68d24bc1a3c7a4e03
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-08-24 16:23:11 +0100

    fix: updated reset-password response model

--- api/v1/schemas/request_password_reset.py
+++ api/v1/schemas/request_password_reset.py
@@ -8,3 +8,9 @@ class RequestEmail(BaseModel):
 class ResetPassword(BaseModel):
     new_password: str = Field(min_length=3)
     confirm_password: str = Field(min_length=3)
+
+
+class ResetPasswordResponse(BaseModel):
+    status_code: int
+    success: bool
+    message: str
