9b52dccbf95d13e979ab180725d7a8277a113f2e
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-08-25 16:09:52 +0100

    Revert "feat: track usage"

--- api/utils/tool_limiter.py
+++ api/utils/tool_limiter.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from api.db.database import get_db
 from api.utils.client_helpers import get_ip_address
-from api.v1.models.usage_store import UsageStore, UsageStore
+from api.v1.models.usage_store import UsageStore
 from api.v1.services.user import user_service
 from api.v1.models.user import User
 from api.v1.services.usage import usage_store_service
@@ -23,35 +23,6 @@ def track_tool_usage(
     user: User | None = Depends(user_service.get_current_user_optional),
 ):
     if user:
-        tracking_record = user_usage_store_service.fetch_by_user(db, user.id)
-        if tracking_record:
-            tracking_record.tool_access_count += 1
-            tool_count = user_usage_store_service.get_or_create_tool_value(
-                db,
-                tracking_record.id,
-                current_tool
-            )
-            if tool_count > ACCESS_LIMIT:
-                raise HTTPException(
-                    status_code=429,
-                    detail="Please upgrade you plan to get more access",
-                )
-            else:
-                user_usage_store_service.update_tool_usage(
-                    db,
-                    tracking_record.id,
-                    current_tool,
-                    tool_count + 1
-                )
-        else:
-            # Create a new record for the user
-            tracking_record = user_usage_store_service.create_usage_store_and_assign_tool(
-                db,
-                client_ip,
-                current_tool,
-                1,1
-            )
-
         return user
     
     client_ip = get_ip_address(request)
@@ -72,16 +43,19 @@ def track_tool_usage(
         else:
             tracking_record.tools_accessed = tracking_record.tools_accessed + [current_tool]                
 
-        else:
-            # Create a new record for the IP
-            usage_store_service.create_usage_store_and_assign_tool(
-                db,
-                client_ip,
-                current_tool,
-                1,1
-            )
-        
-        return None
+    else:
+        # Create a new record for the IP
+        tracking_record = UsageStore(
+            ip_address=client_ip,
+            tool_access_count=1,
+            last_accessed=now,
+            tools_accessed=[current_tool]
+        )
+
+        db.add(tracking_record)
+
+    db.commit()
+    return None
    
 class TrackToolUsage:
     """Class based dependency to allow passing current_tool parameter to dependencies
