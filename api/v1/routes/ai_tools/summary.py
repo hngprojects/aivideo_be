22eb2cb6b19d118e67e47f162e06941acf89dbc1
laban254 <labanrotich6544@gmail.com>
2024-08-26 01:47:24 +0300

    fix: remove audio_scan function due to code breaking issues

--- api/v1/routes/ai_tools/summary.py
+++ api/v1/routes/ai_tools/summary.py
@@ -14,7 +14,7 @@ import io
 from api.db.database import get_db
 from api.utils.success_response import success_response
 from api.utils.files import upload_file_to_current_dir
-from api.utils.files import upload_file, check_file_size, audio_scan
+from api.utils.files import upload_file, check_file_size
 from api.utils.language_code import LANGUAGE_CODES
 from api.v1.services.ai_tools.translator_service import translate_text
 from api.v1.schemas.translation import TranslationRequest
@@ -172,9 +172,6 @@ async def summarize_audio(
         save_extension='mp3' 
     )
     await check_file_size(file)
-    if not await audio_scan(audio_file):
-        raise HTTPException(status_code=400, detail="Audio file scan failed. The file might be corrupted or empty.")
-    
     task_transcribe = transcribe_audio_task.delay(audio_file)
 
 
