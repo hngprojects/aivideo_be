c9bd1aac3f61fe60bafdb81379bb7556dcaaf3c4
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-08-27 13:33:55 +0100

    feat: add response models

--- api/v1/routes/resource.py
+++ api/v1/routes/resource.py
@@ -12,7 +12,9 @@ from api.v1.schemas.resource import (
     CreateResource,
     ResourceBase,
     AllResourcesResponse,
-    UpdateResource
+    UpdateResource,
+    CreateResourceResponse,
+    SuccessResponse
 )
 import logging
 
@@ -200,6 +202,7 @@ async def get_resource_by_id(resource_id: str, db: Annotated[Session, Depends(ge
     "/{resource_id}/publish",
     status_code=status.HTTP_200_OK,
     summary="Publish a resource",
+    response_model=SuccessResponse
 )
 async def publish_resource(
     resource_id: str,
@@ -216,6 +219,7 @@ async def publish_resource(
     "/{resource_id}/unpublish",
     status_code=status.HTTP_200_OK,
     summary="Unpublish a resource",
+    response_model=SuccessResponse
 )
 async def unpublish_resource(
     resource_id: str,
