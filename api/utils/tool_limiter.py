38426c820a9924cac9983907ad5df350514db920
Ayobamidele <ayobamideleewetuga@gmail.com>
2024-08-27 15:33:59 +0100

    updated service to fit model change

--- api/utils/tool_limiter.py
+++ api/utils/tool_limiter.py
@@ -5,9 +5,10 @@ from sqlalchemy.orm import Session
 
 from api.db.database import get_db
 from api.utils.client_helpers import get_ip_address
-from api.v1.models.usage_store import UsageStore
+from api.v1.models.usage_store import UsageStore, UserUsageStore
 from api.v1.services.user import user_service
 from api.v1.models.user import User
+from api.v1.services.user_usage import user_usage_store_service
 
 ACCESS_LIMIT = 3
 TIME_WINDOW = timedelta(days=1)
@@ -21,6 +22,39 @@ def track_tool_usage(
     user: User | None = Depends(user_service.get_current_user_optional),
 ):
     if user:
+        tracking_record = user_usage_store_service.fetch_by_user(db, user.id)
+        if tracking_record:
+            tracking_record.last_accessed = now
+            tracking_record.tool_access_count += 1
+            tool_count = user_usage_store_service.get_or_create_tool_value(
+                db,
+                tracking_record.id,
+                current_tool
+            )
+            if tool_count > ACCESS_LIMIT:
+                raise HTTPException(
+                    status_code=429,
+                    detail="Too many requests, Please upgrade you plan to get more access or wait for tomorrow.",
+                )
+            else:
+                user_usage_store_service.update_tool_usage(
+                    db,
+                    tracking_record.id,
+                    current_tool,
+                    tool_count + 1
+                )
+        else:
+            # Create a new record for the IP
+            tracking_record = UserUsageStore(
+                user_id=user.id,
+                tool_access_count=1,
+                last_accessed=now,
+                tools_accessed={current_tool: 1}
+            )
+
+            db.add(tracking_record)
+            db.commit()
+
         return user
     
     client_ip = get_ip_address(request)
