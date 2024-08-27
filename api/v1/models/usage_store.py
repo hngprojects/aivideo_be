f48da3801093b89e2eb2300806d44ed9f5770831
Ayobamidele <ayobamideleewetuga@gmail.com>
2024-08-27 09:27:02 +0100

    feat: added user usage model for tracking

--- api/v1/models/usage_store.py
+++ api/v1/models/usage_store.py
@@ -2,8 +2,8 @@
 
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, String
-
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from api.v1.models.base_model import BaseTableModel
 from sqlalchemy.dialects.postgresql import ARRAY
 
@@ -25,5 +25,16 @@ class UsageStore(BaseTableModel):
     id = Column(Integer, primary_key=True, autoincrement=True)
     ip_address = Column(String, nullable=False)
     tool_access_count = Column(Integer, default=0)
-    last_accessed = Column(DateTime, default=datetime.utcnow, nullable=False)
-    tools_accessed = Column(ARRAY(String), nullable=True)
+    last_accessed = Column(DateTime, nullable=False)
+    
+    tool_accesses = relationship('ToolAccess', back_populates='usage_store')
+
+class UserUsageStore(BaseTableModel):
+    __tablename__ = "user_usage_store"
+    
+    user_id = Column(String, ForeignKey('users.id', ondelete="CASCADE"))
+    tool_access_count = Column(Integer, default=0)
+    tool_accesses = relationship('ToolAccess', back_populates='usage_store')
+    last_accessed = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    user = relationship('User', back_populates='usage_stored')
