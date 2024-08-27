fcec8bec5da220773a0be0270228552f17fd7f10
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-08-27 12:09:19 +0100

    feat: include fields for tags and cover_image_url in the resource model

--- api/v1/models/resource.py
+++ api/v1/models/resource.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """The Blog Post Model."""
 
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Boolean, ARRAY
 from api.v1.models.base_model import BaseTableModel
 
 
@@ -11,5 +11,7 @@ class Resource(BaseTableModel):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False, comment="has to be a markdown")
     image_url = Column(String)
+    cover_image_url = Column(String)
+    tags = Column(ARRAY(String))
     is_deleted = Column(Boolean, server_default="false")
     is_published = Column(Boolean, server_default="false")
