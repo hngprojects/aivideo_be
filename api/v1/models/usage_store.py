c94238dd9564a7a157c4f48f598edcacaee57f75
Ayobamidele <ayobamideleewetuga@gmail.com>
2024-08-27 09:27:02 +0100

    feat: added user usage model for tracking

--- api/v1/models/usage_store.py
+++ api/v1/models/usage_store.py
@@ -2,25 +2,39 @@
 
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey,JSON
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from api.v1.models.base_model import BaseTableModel
 from sqlalchemy.dialects.postgresql import ARRAY
 
-class UsageStore(BaseTableModel):
-    __tablename__ = "usage_store"
+
+
+class ToolAccess(BaseTableModel):
+    __tablename__ = 'tool_access'
     
-    ip_address = Column(String, index=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    usage_store_id = Column(Integer, ForeignKey('usage_store.id'))
+    tool_name = Column(String, nullable=False)
+    access_count = Column(Integer, default=0)
+    
+    usage_store = relationship('UsageStore', back_populates='tool_accesses')
+
+class UsageStore(BaseTableModel):
+    __tablename__ = 'usage_store'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ip_address = Column(String, nullable=False)
     tool_access_count = Column(Integer, default=0)
-    tools_accessed = Column(JSON, default={})
-    last_accessed = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    last_accessed = Column(DateTime, nullable=False)
+    
+    tool_accesses = relationship('ToolAccess', back_populates='usage_store')
 
 class UserUsageStore(BaseTableModel):
     __tablename__ = "user_usage_store"
     
     user_id = Column(String, ForeignKey('users.id', ondelete="CASCADE"))
     tool_access_count = Column(Integer, default=0)
-    tools_accessed = Column(JSON, default={})
+    tool_accesses = relationship('ToolAccess', back_populates='usage_store')
     last_accessed = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
     user = relationship('User', back_populates='usage_stored')
