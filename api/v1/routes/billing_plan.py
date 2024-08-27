a3340d4aeb624638e291e246ff0bc938ed26351e
Chime <chimeobioha@gmail.com>
2024-08-27 20:40:51 +0100

    Add comment

--- api/v1/routes/billing_plan.py
+++ api/v1/routes/billing_plan.py
@@ -22,7 +22,7 @@ async def create_billing_plan(
     db: Session = Depends(get_db),
 ):
     """
-    Endpoint to create new billing plan by a `superadmin`
+    Endpoint to create new billing plan by a `superadmin`.
     """
 
     new_plan = bp_service.create(db=db, schema=billing_plan_schema)
