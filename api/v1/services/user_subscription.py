1976d0604df6d65adfd5689da8b1073cb536a806
Chime <chimeobioha@gmail.com>
2024-08-27 17:42:28 +0100

    Fix issues across payment and pricing

--- api/v1/services/user_subscription.py
+++ api/v1/services/user_subscription.py
@@ -106,11 +106,11 @@ class UserSubscriptionService:
         db.commit()
     
     @staticmethod
-    def get_sub_start_and_end_datetime(amount_paid, bill_amount, free_plan=False):
+    def get_sub_start_and_end_datetime(amount_paid, bill_per_interval, free_plan=False):
         """Compute and return subcription end datetiem, with start datetime"""
         start_datetime = datetime.now(tz=timezone.utc)
         # If the plan if free_plan, set the duration to 12-months
-        num_of_months = 12 if free_plan else int(amount_paid // bill_amount)
+        num_of_months = 12 if free_plan else int(amount_paid // bill_per_interval)
         num_of_days = num_of_months * 30
         end_datetime = start_datetime + timedelta(days=num_of_days)
         return start_datetime, end_datetime
