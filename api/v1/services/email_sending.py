e8d82f43801106dc1cc652cb241ce9cb0505b5f9
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-09-05 19:51:20 +0100

    fix: update design of email templates

--- api/v1/services/email_sending.py
+++ api/v1/services/email_sending.py
@@ -32,27 +32,15 @@ class EmailSendingService:
     def send_magic_link_email(self, request: Request, background_tasks: BackgroundTasks, user: User, magic_link_url: str):
         '''Thiss function sends the welcome email to a user'''
 
-        # background_tasks.add_task(
-        #     send_email,
-        #     recipient=user.email,
-        #     template_name="magic-link.html",
-        #     subject="Magic Link Authentication",
-        #     context={
-        #         "request": request,
-        #         "user": user,
-        #         "url": magic_link_url
-        #     }
-        # )
-
         background_tasks.add_task(
             send_email,
             recipient=user.email,
-            template_name="welcome-marketing.html",
-            subject="Welcome to TiFi",
+            template_name="magic-link.html",
+            subject="Magic Link Authentication",
             context={
                 "request": request,
                 "user": user,
-                "cta_link": "https://tifi.tv/about"
+                "url": magic_link_url
             }
         )
     
