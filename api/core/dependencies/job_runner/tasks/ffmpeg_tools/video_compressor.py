25a7938052614c8d62e6d4ef131cecc18e5b6d8d
Adegbehingbe Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-11-11 00:31:39 +0100

    fix: update progress writing to be more granular for ffmpeg tools

--- api/core/dependencies/job_runner/tasks/ffmpeg_tools/video_compressor.py
+++ api/core/dependencies/job_runner/tasks/ffmpeg_tools/video_compressor.py
@@ -9,7 +9,7 @@ from api.utils.settings import settings
 from api.utils import mime_types
 from api.v1.services.job import tifi_job_service
 from api.v1.services.tools.ffmpeg_tools import ffmpeg_service
-from api.core.dependencies.job_runner.app.utils import save_and_print_job_progress
+from api.core.dependencies.job_runner.app.utils import run_ffmpeg_with_progress, save_and_print_job_progress
 
 db = next(get_db())
 
@@ -24,18 +24,20 @@ video_url = payload.get('video_url')
 compression_speed = payload.get('compression_speed')
 
 # Download video file from minio
-save_and_print_job_progress(db, job, 10, f'Downloading and opening video file from {video_url}')
+save_and_print_job_progress(db, job, 0, f'Downloading and opening video file from {video_url}')
 video_file = minio_service.download_file_from_minio(video_url)
 
 try:
     save_and_print_job_progress(db, job, 40, f'Compressing video')
     # Use ffmpeg to extract audio from the video
-    output_video = ffmpeg_service.compress_video(
+    cmd, duration, output_video = ffmpeg_service.compress_video(
         input_video=video_file,
         compression_speed=compression_speed,
     )
+    
+    run_ffmpeg_with_progress(db, job, 'Compressing video', cmd, duration)
 
-    save_and_print_job_progress(db, job, 70, f'Generating preview and download links for compressed video')
+    save_and_print_job_progress(db, job, 100, f'Generating preview and download links for compressed video')
     save_url, download_url = minio_service.upload_to_minio(
         folder_name='video-compressor',
         source_file=output_video,
@@ -43,16 +45,16 @@ try:
         content_type=mime_types.VIDEO_MP4
     )
 
-    save_and_print_job_progress(db, job, 80, 'Cleaning up')
+    save_and_print_job_progress(db, job, 100, 'Cleaning up')
     delete_file(output_video)
 
-    save_and_print_job_progress(db, job, 90, 'Generating result')
+    save_and_print_job_progress(db, job, 100, 'Generating result')
     result = {
         'preview_url': save_url,
         'download_url': download_url,
     }
 
-    save_and_print_job_progress(db, job, 95)
+    save_and_print_job_progress(db, job, 100)
     print(json.dumps(result))
 
 except ffmpeg.Error as ffmpeg_error:
--- api/core/dependencies/job_runner/tasks/ffmpeg_tools/video_watermarker.py
+++ api/core/dependencies/job_runner/tasks/ffmpeg_tools/video_watermarker.py
@@ -9,7 +9,7 @@ from api.utils.settings import settings
 from api.utils import mime_types
 from api.v1.services.job import tifi_job_service
 from api.v1.services.tools.ffmpeg_tools import ffmpeg_service
-from api.core.dependencies.job_runner.app.utils import save_and_print_job_progress
+from api.core.dependencies.job_runner.app.utils import run_ffmpeg_with_progress, save_and_print_job_progress
 
 db = next(get_db())
 
@@ -25,22 +25,24 @@ watermark_image_url = payload.get('watermark_image_url')
 position = payload.get('position')
 
 # Download video file from minio
-save_and_print_job_progress(db, job, 10, f'Downloading and opening video file from {video_url}')
+save_and_print_job_progress(db, job, 0, f'Downloading and opening video file from {video_url}')
 video_file = minio_service.download_file_from_minio(video_url)
 
 # Download watermark image from minio
-save_and_print_job_progress(db, job, 20, f'Downloading and opening watermark image file from {watermark_image_url}')
+save_and_print_job_progress(db, job, 0, f'Downloading and opening watermark image file from {watermark_image_url}')
 watermark_image = minio_service.download_file_from_minio(watermark_image_url)
 
 try:
     save_and_print_job_progress(db, job, 40, f'Creating video with watermark')
-    output_video = ffmpeg_service.add_watermark_to_video(
+    cmd, duration, output_video = ffmpeg_service.add_watermark_to_video(
         input_video=video_file,
         watermark_image=watermark_image,
         position=position
     )
+    
+    run_ffmpeg_with_progress(db, job, 'Creating video with watermark', cmd, duration)
 
-    save_and_print_job_progress(db, job, 70, f'Generating preview and download links for watermarked video')
+    save_and_print_job_progress(db, job, 100, f'Generating preview and download links for watermarked video')
     save_url, download_url = minio_service.upload_to_minio(
         folder_name='watermark-adder',
         source_file=output_video,
@@ -48,16 +50,16 @@ try:
         content_type=mime_types.VIDEO_MP4
     )
 
-    save_and_print_job_progress(db, job, 80, 'Cleaning up')
+    save_and_print_job_progress(db, job, 100, 'Cleaning up')
     delete_file(output_video)
 
-    save_and_print_job_progress(db, job, 90, 'Generating result')
+    save_and_print_job_progress(db, job, 100, 'Generating result')
     result = {
         'preview_url': save_url,
         'download_url': download_url,
     }
 
-    save_and_print_job_progress(db, job, 95)
+    save_and_print_job_progress(db, job, 100)
     print(json.dumps(result))
 
 except ffmpeg.Error as ffmpeg_error:
